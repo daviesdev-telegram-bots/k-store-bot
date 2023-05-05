64bf01bf68c5ac718d6dbb520cdd48b2ecf5d6c8
xandee <davidonzan@gmail.com>
2023-05-06 00:20:28 +0100

    Admin change categories & Bug fixes

--- bot.py
+++ bot.py
@@ -21,10 +21,10 @@ def productkb(i, cat):
     kb = InlineKeyboardMarkup()
     kb.add(InlineKeyboardButton("Buy", pay=True), InlineKeyboardButton("Add to cart🛒", callback_data=f"+cart{i}"))
     if i != fproduct.id:
-        prev = InlineKeyboardButton("🔼 Previous", callback_data=f"p{category[index-1].id}")
+        prev = InlineKeyboardButton("🔼 Previous", callback_data=f"cp{cat}:{category[index-1].id}")
         kb.add(prev)
     if i != lproduct.id:
-        next_ = InlineKeyboardButton("🔽 Next", callback_data=f"p{category[index+1].id}")
+        next_ = InlineKeyboardButton("🔽 Next", callback_data=f"cp{cat}:{category[index+1].id}")
         kb.add(next_)
     return kb
 
@@ -62,24 +62,72 @@ def callback_handler(call: CallbackQuery):
             bot.register_next_step_handler(message, add_product)
         elif data in ["products", "prod_back"]:
             bot.edit_message_text("Select Product", message.chat.id, message.id, reply_markup=Admin.get_products())
+        
+        elif data in ["categories", "cat_back"]:
+            bot.edit_message_text("Select a category", message.chat.id, message.id, reply_markup=Admin.get_categories())
+        
+        elif data == "home":
+            bot.edit_message_text("<b>Hello Admin !</b> What will you like to edit today?", message.chat.id, message.id, reply_markup=Admin.keyboard)
+
         elif data.startswith("p") and data[1:].isdigit():
             product = session.query(Product).get(int(data[1:]))
             bot.edit_message_text(f"Edit Product\nName: {product.name}\nPrice: {product.price}\nDescription: {product.description}", message.chat.id, message.id, reply_markup=Admin.edit_product(data[1:]))
-        elif data.startswith("e") and data[1] in ["n", "p", "d"]:
+
+        elif data.startswith("editcat"):
+            _, cat_id = data.split(":")
+            category = session.query(Category).get(cat_id)
+            bot.edit_message_text(f"Name: {category.name}", message.chat.id, message.id, reply_markup=Admin.edit_category(cat_id))
+
+        elif data.startswith("e") and data[1] in ["n", "p", "d", "c", "i"]:
             product_id = data[3:]
+            if data[1] == "c":
+                bot.edit_message_text(f"Select the category you want to transfer the product to", message.chat.id, message.id, reply_markup=Admin.edit_product_category(product_id))
+                return
+            elif data[1] == "i":
+                bot.edit_message_text(f"Send the image you want to use", message.chat.id, message.id, reply_markup=InlineKeyboardMarkup().add(Admin.back))
+                bot.register_next_step_handler(message, edit_image, product_id)
+                return
             n = {"n":"name", "p":"price", "d": "description"}
             bot.send_message(message.chat.id, f"Send the new {n[data[1]]}")
             bot.register_next_step_handler(message, edit_product, data[1], product_id)
+        
         elif data.startswith("create_cat"):
             bot.send_message(message.chat.id, "What is the name of the category?")
             bot.register_next_step_handler(message, new_category)
-            
+
+        elif data.startswith("chancatname"):
+            _, cat_id = data.split(":")
+            bot.send_message(message.chat.id, f"What do you want to change the category name to?")
+            bot.register_next_step_handler(message, change_category_name, cat_id)
+
+        elif data.startswith("del"):
+            _, prod_id = data.split(":")
+            name = session.query(Product).get(int(prod_id)).name
+            kb = InlineKeyboardMarkup()
+            kb.add(InlineKeyboardButton("✅Yes", callback_data=f"confirm_del:{prod_id}"), InlineKeyboardButton("❌ No", callback_data=f"admin_prod_back"))
+            bot.edit_message_text("Are you sure you want to delete "+name.upper(), message.chat.id, message.id, reply_markup=kb)
+
+        elif data.startswith("confirm_del"):
+            _, prod_id = data.split(":")
+            prod = session.query(Product).get(int(prod_id))
+            session.delete(prod)
+            session.commit()
+            kb = InlineKeyboardMarkup().add(Admin.back)
+            bot.edit_message_text(prod.name+" has been deleted", message.chat.id, message.id, reply_markup=kb)
+
+        elif data.startswith("chan_cat"):
+            _, prod_id, cat_id = data.split(":")
+            product = session.query(Product).get(int(prod_id))
+            product.category = cat_id
+            session.commit()
+            kb = InlineKeyboardMarkup().add(Admin.back)
+            bot.edit_message_text(f"{product.name}'s category has been updated!", message.chat.id, message.id, reply_markup=kb)
+
         elif data.startswith("nprod_cat"):
             _, prod_id, cat_id = data.split(":")
-            session.query(Product).get(prod_id).category = cat_id
-            bot.send_message(message.chat.id, "Product added successfully!!!")
+            session.query(Product).get(int(prod_id)).category = cat_id
             session.commit()
-            admin(message)
+            bot.send_message(message.chat.id, "Product added successfully!!!", reply_markup=InlineKeyboardMarkup().add(Admin.back))
 
 def new_category(message):
     if message.text:
@@ -87,11 +135,21 @@ def new_category(message):
         category = Category(name=message.text)
         session.add(category)
         session.commit()
-        bot.send_message(message.chat.id, "Category created")
-        admin(message)
+        bot.send_message(message.chat.id, "Category created", reply_markup=InlineKeyboardMarkup().add(Admin.cat_back))
     else:
         bot.send_message(message.chat.id, "Please send a text message")
         bot.register_next_step_handler(message, new_category)
+
+def change_category_name(message, cat_id):
+    if message.text:
+        if is_cancel(message): return
+        category = session.query(Category).get(cat_id)
+        category.name = message.text
+        session.commit()
+        bot.send_message(message.chat.id, "Category name modified to "+message.text, reply_markup=InlineKeyboardMarkup().add(Admin.cat_back))
+    else:
+        bot.send_message(message.chat.id, "Please send a text message")
+        bot.register_next_step_handler(message, change_category_name)
 
 def add_product(message):
     if is_cancel(message): return
@@ -121,13 +179,7 @@ def random_string():
 
 def add_image(message:Message, details):
     if is_cancel(message): return
-    download_file_path = bot.get_file(message.photo[1].file_id).file_path
-    save_file_path = ""+download_file_path
-    while os.path.isfile(save_file_path):
-        save_file_path += random_string()
-    with open(save_file_path, "wb") as f:
-        f.write(bot.download_file(download_file_path))
-    details["image"] = save_file_path
+    details["image"] = message.photo[1].file_id
     product = Product(**details)
     session.add(product)
     session.commit()
@@ -138,7 +190,6 @@ def add_image(message:Message, details):
 
 def edit_product(message, pty, pid):
     product = session.query(Product).get(pid)
-    n = {"n":product.name, "p":product.price, "d": product.description}
     if pty == "n":
         product.name = message.text
     elif pty == "d":
@@ -154,6 +205,13 @@ def edit_product(message, pty, pid):
     bot.send_message(message.chat.id, "Product updated !!!")
     bot.send_message(message.chat.id, f"Edit Product\nName: {product.name}\nPrice: {product.price}\nDescription: {product.description}", reply_markup=Admin.edit_product(pid))
 
+def edit_image(message, prod_id):
+    if is_cancel(message): return
+    p = session.query(Product).get(prod_id)
+    p.image = message.photo[1].file_id
+    session.commit()
+    bot.send_message(message.chat.id, p+" Image has been replaced!", reply_markup=InlineKeyboardMarkup().add(Admin.back))
+
 def is_cancel(message):
     if message.text in ["/start", "/cancel", "/admin"]:
         bot.send_message(message.chat.id, "Operation cancelled")
@@ -165,7 +223,7 @@ def is_cancel(message):
 def send_invoice(message:Message, product:Product, cat):
     bot.send_invoice(
         message.chat.id, product.name, product.description,
-        "true", stripe_token, "usd", [LabeledPrice(product.name, int(product.price*100))], reply_markup=productkb(product.id, cat))
+        "true", stripe_token, "usd", [LabeledPrice(product.name, int(product.price*100))], photo_url=bot.get_file_url(product.image) if product.image else None, reply_markup=productkb(product.id, cat))
 
 bot.enable_save_next_step_handlers(1)
 print("Started")
--- kb.py
+++ kb.py
@@ -17,18 +17,24 @@ class Customer:
         categorykb = InlineKeyboardMarkup(row_width=2)
         categories = session.query(Category).all()
         for cat in categories:
-            btn = InlineKeyboardButton(cat.name, callback_data=f"cp{cat.id}:{cat.products[0].id}")
-            categorykb.add(btn)
+            if len(cat.products) > 0:
+                btn = InlineKeyboardButton(cat.name, callback_data=f"cp{cat.id}:{cat.products[0].id}")
+                categorykb.add(btn)
         return categorykb
 
 
 class Admin:
+    back = InlineKeyboardButton("Back", callback_data=f"admin_prod_back")
+    cat_back = InlineKeyboardButton("Back", callback_data=f"admin_cat_back")
+    home = InlineKeyboardButton("Back", callback_data=f"admin_home")
     keyboard = InlineKeyboardMarkup()
     add_product = InlineKeyboardButton("Add Product", callback_data="admin_newproduct")
-    products = InlineKeyboardButton("Products", callback_data="admin_products")
+    products = InlineKeyboardButton("View Products", callback_data="admin_products")
+    categories = InlineKeyboardButton("View Categories", callback_data="admin_categories")
     keyboard.add(add_product)
-    keyboard.add(products)
     keyboard.add(InlineKeyboardButton("Create category", callback_data="admin_create_cat"))
+    keyboard.add(products)
+    keyboard.add(categories)
 
     @staticmethod
     def get_products():
@@ -36,16 +42,47 @@ class Admin:
         for prod in session.query(Product).all():
             product = InlineKeyboardButton(prod.name, callback_data=f"admin_p{prod.id}")
             kb.add(product)
+        kb.add(Admin.home)
         return kb
     
     @staticmethod
     def edit_product(product_id):
-        kb = InlineKeyboardMarkup()
+        kb = InlineKeyboardMarkup(row_width=2)
         edit_name = InlineKeyboardButton("Edit Name", callback_data=f"admin_en_{product_id}")
         edit_price = InlineKeyboardButton("Edit Price", callback_data=f"admin_ep_{product_id}")
-        edit_desc = InlineKeyboardButton("Edit Description", callback_data=f"admin_ed{product_id}")
-        back = InlineKeyboardButton("Back", callback_data=f"admin_prod_back")
-        kb.add(edit_name, edit_price, edit_desc)
-        kb.add(back)
+        edit_desc = InlineKeyboardButton("Edit Description", callback_data=f"admin_ed_{product_id}")
+        edit_image = InlineKeyboardButton("Replace Image", callback_data=f"admin_ei_{product_id}")
+        edit_category = InlineKeyboardButton("Edit Category", callback_data=f"admin_ec_{product_id}")
+        delete = InlineKeyboardButton("❌ DELETE", callback_data=f"admin_del:{product_id}")
+        kb.add(edit_name, edit_price, edit_desc, edit_category, edit_image)
+        kb.add(delete)
+        kb.add(Admin.back)
+        return kb
+    
+    def edit_category(cat_id):
+        kb = InlineKeyboardMarkup(row_width=2)
+        edit_name = InlineKeyboardButton("Edit Name", callback_data=f"admin_chancatname:{cat_id}")
+        # delete = InlineKeyboardButton("❌ DELETE", callback_data=f"admin_catdel:{cat_id}")
+        kb.add(edit_name)
+        # kb.add(delete)
+        kb.add(Admin.cat_back)
+        return kb
+    
+    @staticmethod
+    def get_categories():
+        categorykb = InlineKeyboardMarkup()
+        categories = session.query(Category).all()
+        for cat in categories:
+            btn = InlineKeyboardButton(cat.name, callback_data=f"admin_editcat:{cat.id}")
+            categorykb.add(btn)
+        categorykb.add(Admin.home)
+        return categorykb
+
+    @staticmethod
+    def edit_product_category(product_id):
+        kb = InlineKeyboardMarkup(row_width=2)
+        categories = session.query(Category).all()
+        kb.add(*[InlineKeyboardButton(i.name, callback_data=f"admin_chan_cat:{product_id}:{i.id}") for i in categories])
+        kb.add(Admin.back)
         return kb
     
